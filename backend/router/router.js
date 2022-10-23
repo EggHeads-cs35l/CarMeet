dcc95d06eb80591c4cc44200e5663f6cec397f23
matthewliu0324 <matthewliu453@gmail.com>
2022-10-23 13:25:40 -0700

    Fixed syntax issue and .env file problem

--- backend/router/router.js
+++ backend/router/router.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const signupTemp = require("../models/signup");
-const recipTemp = require('../models/recipients');
 const { response } = require("express");
 const spawn = require("child_process").spawn;
 
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const routeUrls = require('./backend/router/router');
+const routeUrls = require('./router/router');
 const cors = require('cors');
 
 dotenv.config();
-
 mongoose.connect(process.env.DATABASE_ACCESS, ()=>{console.log("connected")});
 
 app.use(express.json());
