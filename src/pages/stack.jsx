fa653f6eb6197661d1c3c22059fccdce2a7fa4ad
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-11-25 01:03:58 -0800

    basic stack profiles

--- src/pages/stack.jsx
+++ src/pages/stack.jsx
@@ -11,10 +11,10 @@ export default function Stack() {
       <Card style={{width:'50rem', height:'auto'}}>
         <Card.Img src={image} />
         <Card.Body>
-          <Card.Title><b>"First Last"</b></Card.Title>
-          <Card.Subtitle>"California"</Card.Subtitle>
+          <Card.Title><h1>"First Last"</h1></Card.Title>
+          <Card.Subtitle class="mb-2 text-muted"><h3>"State"</h3></Card.Subtitle>
           <ListGroup variant="flush">
-            <ListGroup.Item>"year"+"make"+"model"</ListGroup.Item>
+            <ListGroup.Item class="mb-2"><h2 style={{position: 'absolute', left: '50%', top: '50%',transform: 'translate(-50%, -50%)'}}>"year"+"make"+"model"</h2></ListGroup.Item>
           </ListGroup>
         </Card.Body>
       </Card>
