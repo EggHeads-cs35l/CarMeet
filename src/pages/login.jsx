3ab2ee8a88a878a664e7e70102815b823600c906
MariusPL <mario.penglee.mpl@gmail.com>
2022-11-27 19:55:45 -0800

    integrated imagenet

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,41 @@
-
+import 'babel-polyfill';
 import React, { useState } from "react"
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-backend-webgl';
+
+import {CarNoCar} from './model';
+import imageURL from './cat.jpeg';
 
 export default function (props) {
   let [authMode, setAuthMode] = useState("signin")
-
   const changeAuthMode = () => {
     setAuthMode(authMode === "signin" ? "signup" : "signin")
   }
+
+  const verifyImage = async () => {
+    const img = document.getElementById('img');
+    img.src = imageURL;
+    await img;
+    const resultElement = document.getElementById('result');
+    resultElement.innerText = 'Loading CNN model...';
+
+    const imageModel = new CarNoCar();
+    console.time('Loading of model');
+    await imageModel.load();
+    console.timeEnd('Loading of model');
+    const pixels = tf.browser.fromPixels(img);
+    //const results = imageModel.execute(pixels);
+    console.time('First prediction');
+    let result = imageModel.predict(pixels);
+    const topK = imageModel.getTopKClasses(result, 2);
+    console.timeEnd('First prediction');
+    resultElement.innerText = '';
+  topK.forEach(x => {
+    resultElement.innerText += `${x.value.toFixed(3)}: ${x.label}\n`;
+  });
+    imageModel.dispose();
+  }
+
 
   if (authMode === "signin") {
     return (
@@ -68,6 +97,11 @@ export default function (props) {
           </div>
           <div className="form-group mt-3">
             <label>Profile Image</label>
+            <img id="img"></img>
+            <div id="result"></div>
+            <span className="link-primary" onClick={verifyImage}>
+              <u>Verify Image</u>
+            </span>
             <input
               type="file"
               accept=".jpeg, .png"
