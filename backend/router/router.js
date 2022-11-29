9b433813ee369f90d9d0581f0f1ac7debfccadd4
Haotian Yi <yi2021@g.ucla.edu>
2022-11-29 15:12:27 -0800

    Delete deprecated backend directory

--- backend/router/router.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { response } = require("express");
-const spawn = require("child_process").spawn;
-
-module.exports = router;
