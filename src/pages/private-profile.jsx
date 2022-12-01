9c8e28ee09eb21fb77bd1aaecaa1365bf6a2974c
MariusPL <mario.penglee.mpl@gmail.com>
2022-12-01 02:16:07 -0800

    inbox

--- src/pages/private-profile.jsx
+++ src/pages/private-profile.jsx
@@ -6,6 +6,10 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style/profile.css";
 import Button from "react-bootstrap/Button";
 import { BsFillInboxesFill, BsArrowBarLeft } from "react-icons/bs";
+import {Component} from "react"
+
+import Modal from 'react-bootstrap/Modal';
+import { useState } from "react";
 
 function inbox(){
   return(
@@ -16,18 +20,63 @@ function inbox(){
 }
 
 export default function Profile() {
+  const messages = [("hello world", "user 1"), ("hello world", "user 2"), ("hello world", "user 3")];
+  const listMessages = messages.map((message) =>
+    <div key={message.toString()}>
+      {message}
+    </div>
+  );
+
   const location = useLocation();
   const userData = location.state;
   const navigate = useNavigate();
+
+
+  //modal stuff
+  const [show, setShow] = useState(false);
+
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleSend = () => {
+    alert("Message sent. You will be notified when the user responds.");
+  }
+  //modal stuff end
+
   return (
     <div className="profile">
+    <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Inbox</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{
+                maxHeight: 'calc(100vh - 210px)',
+                overflowY: 'auto'
+                          }}
+                          >
+          <form>
+          <div>
+          {listMessages}
+          </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+      </Modal>
+
     <div style={{ position: "absolute", left: "17%", top: "5%" }}>
           <Button variant="outline-danger" size="lg" onClick={() => navigate(-1)}>
             <BsArrowBarLeft class="mb-1" />
           </Button>
     </div>
+
     <div style={{ position: "absolute", right: "17%", top: "5%" }}>
-          <Button size="lg" onClick={inbox}>
+          <Button size="lg" onClick={handleShow}>
             <BsFillInboxesFill class="mb-1" />
           </Button>
     </div>
@@ -64,3 +113,14 @@ export default function Profile() {
     </div>
   );
 }
+
+class George extends Component {
+    render() {
+        return (
+            <div>
+                card html
+            </div>
+        )
+    }
+}
+
