f5c0a7a50d6754fc9d8dca4a1d1808373f58619c
matthewliu0324 <matthewliu453@gmail.com>
2022-11-30 20:10:03 -0800

    updated serach

--- src/backend/API/search.js
+++ src/backend/API/search.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
 const Search = (setData, props = null) =>{
-   axios.post("http://131.179.49.113:4000/app/search", {})
+   axios.post("http://131.179.49.113:4000/app/search", props)
    .then((response) => {
       if (response.data != null){
          const users = Array()
          for (var user of response.data){
             users.push(user)
          }
+         console.log(users)
          setData(users);
       }
-    });
+    }); 
 }
 
 export default Search;
