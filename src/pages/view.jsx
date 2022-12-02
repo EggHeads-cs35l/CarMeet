2dd59fd1ac2357bf059d4aadf88aba2aa8116fb6
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-12-02 01:00:18 -0800

    added back button to view

--- src/pages/view.jsx
+++ src/pages/view.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import ProfilePublic from '../pages/public-profile.jsx'
-import {useLocation} from 'react-router-dom'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { BsArrowBarLeft } from "react-icons/bs";
+import { useLocation, useNavigate } from "react-router-dom";
+import ProfilePublic from "../pages/public-profile.jsx";
 
 export default function View() {
   const location = useLocation();
   const value = location.state;
+  const navigate = useNavigate();
   return (
-    <div>
-      <ProfilePublic className="profile" name={value.name} state={value.state} year={value.year} make={value.make} model={value.model} img1={value.img1} />
-    </div>
-  )
+    <>
+      <div style={{ position: "absolute", left: "18%", top: "18.5%" }}>
+        <Button variant="outline-danger" size="lg" onClick={() => navigate(-1)}>
+          <BsArrowBarLeft class="mb-1" />
+        </Button>
+      </div>
+      <div>
+        <ProfilePublic
+          className="profile"
+          name={value.name}
+          state={value.state}
+          year={value.year}
+          make={value.make}
+          model={value.model}
+          img1={value.img1}
+        />
+      </div>
+    </>
+  );
 }
